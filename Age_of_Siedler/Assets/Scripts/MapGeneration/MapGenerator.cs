e8eeaf3592e286ffe181bf6e3dd1300962c4b851
Maximilian Dorn <maximilian-dorn@web.de>
2019-11-13 14:15:27 +0100

    [ADDED] Wolrd/Map Generation Script

---
+++ Age_of_Siedler/Assets/Scripts/MapGeneration/MapGenerator.cs
@@ -0,0 +1,152 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour
+{
+    public string seed;
+    public bool useRandomSeed;
+
+    public GameObject grasPrefab;
+    public GameObject waterPrefab;
+
+    int[,] map;
+
+    [Range(0, 100)]
+    public int randomFillPercent;
+
+    public int width;
+    public int height;
+
+    public int normalizeCount;
+
+
+    private void Start()
+    {
+        GenerateMap();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GenerateMap();
+        }
+    }
+
+    void GenerateMap()
+    {
+        map = new int[width,height];
+        RandomFillMap();
+
+        for (int i = 0; i < normalizeCount; i++)
+        {
+            NormalizeMap();
+        }
+    }
+
+    void RandomFillMap()
+    {
+        if (useRandomSeed)
+        {
+            seed = Time.time.ToString();
+        }
+
+        System.Random randomNumber = new System.Random(seed.GetHashCode());
+
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                if(x==0 || x==width-1 ||y==0 || y== height - 1)
+                {
+                    map[x, y] = 1;
+                }
+                else
+                {
+                    map[x, y] = (randomNumber.Next(0, 100) < randomFillPercent) ? 1 : 0;
+                }
+            }
+        }
+    }
+
+    void NormalizeMap()
+    {
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                int neighborWallTiles = GetNeighbors(x, y);
+
+                    if(neighborWallTiles > 4)
+                    {
+                        map[x, y] = 1;
+                    }
+                    else if (neighborWallTiles < 4)
+                    {
+                        map[x, y] = 0;
+                    }
+
+            }
+        }
+    }
+
+    int GetNeighbors(int gridX, int gridY)
+    {
+        int wallCount = 0;
+        for (int x = gridX-1; x <= gridX+1; x++)
+        {
+            for (int y = gridY - 1; y <= gridY + 1; y++)
+            {
+                if(x >= 0 && y < width && y >= 0 && y < height)
+                {
+                    if(x != gridX || y != gridY)
+                    {
+                        wallCount += map[x, y];
+                    }
+                }
+                else
+                {
+                    wallCount++;
+                }
+            }
+        }
+
+        return wallCount;
+    }
+
+    private void OnDrawGizmos()
+    {
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                Gizmos.color = (map[x, y] == 1) ? Color.black : Color.white;
+                Vector3 pos = new Vector3(-width / 2 + x + .5f, 0, -height / 2 + y + .5f);
+                Gizmos.DrawCube(pos, Vector3.one);
+            }
+        }
+    }
+
+    //private void FillMap()
+    //{
+    //    for (int x = 0; x < width; x++)
+    //    {
+    //        for (int y = 0; y < height; y++)
+    //        {
+    //            GameObject block;
+    //            if(map[x,y] == 1)
+    //            {
+    //                block = Instantiate(grasPrefab, transform, false);
+    //                block.transform.localPosition = new Vector3(x, y, 0);
+    //            }
+    //            else
+    //            {
+    //                block = Instantiate(waterPrefab, transform, false);
+    //                block.transform.localPosition = new Vector3(x, y, 0);
+    //            }
+    //        }
+    //    }
+    //}
+}
---
+++ Age_of_Siedler/Assets/Scripts/MapGeneration/ReworkedMapGenerator.cs
@@ -0,0 +1,182 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class ReworkedMapGenerator : MonoBehaviour
+{
+    public GameObject grasBlock = null;
+    public GameObject waterBlock = null;
+
+    public string seed;
+    public bool useRandomSeed;
+
+    public const bool GROUND = true;
+    public const bool WALL = false;
+
+    private bool[,] map;
+    private Transform[,] gameMap;
+
+    public int width;
+    public int height;
+
+    [Range(0, 100)]
+    public int randomFillPercent;
+
+    public int normalizeCount = 4;
+
+
+    public bool IsGround(int x, int y)
+    {
+        return map[x, y];
+    }
+
+    private void Start()
+    {
+        GenerateMap();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+
+            GenerateMap();
+        }
+    }
+
+    void GenerateMap()
+    {
+        //map = new bool[width, height];
+        InstantiateMapValues();
+        
+        FillMap();
+    }
+
+
+
+    public void InstantiateMapValues()
+    {
+        
+        if (useRandomSeed)
+        {
+            seed = Time.time.ToString();
+        }
+
+        System.Random random = new System.Random(seed.GetHashCode());
+
+        map = new bool[width, height];
+
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                if(x == 0 || x == width - 1 || y == 0 || y == height - 1)
+                {
+                    map[x, y] = WALL;
+                }
+                else
+                {
+                    var chance = random.Next(0, 100);
+                    if(chance > randomFillPercent)
+                    {
+                        map[x, y] = GROUND;
+                    }
+                    else
+                    {
+                        map[x, y] = WALL;
+                    }
+                }
+            }
+        }
+        NormalizeMap(normalizeCount);
+    }
+
+    public void FillMap()
+    {
+        gameMap = new Transform[width, height];
+
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                bool state = IsGround(x, y);
+                GameObject block;
+                if (state)
+                {
+                    block = Instantiate(grasBlock, transform, false);
+                    block.transform.localPosition = new Vector3(x, y, 0);
+                }
+                else
+                {
+                    block = Instantiate(waterBlock, transform, false);
+                    block.transform.localPosition = new Vector3(x, y, 0);
+                }
+                gameMap[x, y] = block.transform;
+            }
+        }
+    }
+
+    public void NormalizeMap(int rounds)
+    {
+        var temp = new bool[width, height];
+        for (int i = 0; i < rounds; i++)
+        {
+            for (int x = 1; x < width - 1; x++)
+            {
+                for (int y = 1; y < height - 1; y++)
+                {
+                    int wall = CountNeighbours(x, y);
+                    if (map[x, y])
+                    {
+                        if(wall > 4)
+                        {
+                            temp[x, y] = WALL;
+                        }
+                        else
+                        {
+                            temp[x, y] = GROUND;
+                        }
+                    }
+                    else
+                    {
+                        if(wall < 2)
+                        {
+                            temp[x, y] = GROUND;
+                        }
+                        else
+                        {
+                            temp[x, y] = WALL;
+                        }
+                    }
+                }
+            }
+        var copy = map;
+        map = temp;
+        temp = copy;
+
+        }
+    }
+
+    private int CountNeighbours(int x, int y)
+    {
+        int wallCount = 0;
+
+        for (int gridX = -1; gridX < 2; gridX++)
+        {
+            for (int gridY = -1; gridY < 2; gridY++)
+            {
+                if (gridX == 0 && gridY == 0)
+                {
+                    continue;
+                }
+                if (!map[x + gridX, y + gridY])
+                {
+                    wallCount += 1;
+                }
+            }
+        }
+
+        return wallCount;
+    }
+}
