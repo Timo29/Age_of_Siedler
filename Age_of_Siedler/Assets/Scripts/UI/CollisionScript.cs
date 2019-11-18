aa9cd73bc6badf66057ce588058715000d82bde7
KrAn3r <lexalp59@gmail.com>
2019-11-18 16:38:10 +0100

    [ADD]CollisionScript

---
+++ Age_of_Siedler/Assets/Scripts/UI/CollisionScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionScript : MonoBehaviour
+{
+
+    private SpawnHouseScript spawnHouse;
+
+
+
+    private void Start()
+    {
+        spawnHouse = GameObject.FindGameObjectWithTag("UIManager").GetComponent<SpawnHouseScript>();
+
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("house"))
+        {
+            spawnHouse.spawn = false;
+            print("Working"); 
+        }
+    }
+
+}
