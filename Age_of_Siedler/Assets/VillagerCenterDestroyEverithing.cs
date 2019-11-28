a56413f955b33942342285c05635697a6909952f
Timo <timo@team-stoeckmann.de>
2019-11-28 18:29:01 +0100

    [ADD] Destroy Radius Script

---
+++ Age_of_Siedler/Assets/VillagerCenterDestroyEverithing.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VillagerCenterDestroyEverithing : MonoBehaviour
+{
+    Transform transform;
+    Collider[] toDestroyObjects;
+    public float radius;
+    public LayerMask destroyObjectsLayer;
+
+    void Start()
+    {
+        transform = GetComponent<Transform>();
+    }
+
+    private void FixedUpdate()
+    {
+        toDestroyObjects = Physics.OverlapSphere(transform.position, radius, destroyObjectsLayer);
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            DestroyAll();
+        }
+    }
+
+    void DestroyAll()
+    {
+        for (int i = 0; i < toDestroyObjects.Length; i++)
+        {
+            Destroy(toDestroyObjects[i].gameObject);
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(transform.position, radius);
+    }
+}
